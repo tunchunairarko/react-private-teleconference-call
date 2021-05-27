f3e3239e7d530929fcd7e34b6e80fbe130f50357
tunchunairarko <tunchunairarko@gmail.com>
2021-05-27 23:53:56 +0200

    c

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,31 @@ import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Home = props =>
-  <div className="home">
-    <div>
-      <h1 itemProp="headline">HWU Telepresence Room</h1>
-      <p>Here is the unique room ID.</p>
-      <input type="text" name="room" value={ props.roomId } onChange={props.handleChange} pattern="^\w+$" maxLength="10" required autoFocus title="Room name should only contain letters or numbers."/>
-      <center>
-      <Link className="primary-button" to={ '/r/' + props.roomId }>Join</Link>
-      </center>
-      {/* <Link className="primary-button" to={ '/r/' + props.defaultRoomId }>Random</Link> */}
-      { props.rooms.length !== 0 && <div>Recently used rooms:</div> }
-      { props.rooms.map(room => <Link key={room} className="recent-room" to={ '/r/' + room }>{ room }</Link>) }
-    </div>
-  </div>;
+class Home extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  
+
+  render(){
+    return(
+      <div className="home">
+        <div>
+          <h1 itemProp="headline">HWU Telepresence Room</h1>
+          <p>Your unique room ID.</p>
+          <input type="text" name="room" value={ this.props.roomId } onChange={this.props.handleChange} pattern="^\w+$" maxLength="10" required autoFocus title="Room name should only contain letters or numbers."/>
+          <center>
+          <Link className="primary-button" to={ '/r/' + this.props.roomId }>Join</Link>
+          </center>
+          {/* <Link className="primary-button" to={ '/r/' + props.defaultRoomId }>Random</Link> */}
+          { this.props.rooms.length !== 0 && <div>Recently used rooms:</div> }
+          { this.props.rooms.map(room => <Link key={room} className="recent-room" to={ '/r/' + room }>{ room }</Link>) }
+        </div>
+      </div>
+    )
+  }
+}
+ 
 
 Home.propTypes = {
   handleChange: PropTypes.func.isRequired,
