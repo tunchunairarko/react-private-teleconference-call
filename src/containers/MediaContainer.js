4ca2044bf31c359005ff8662ccd3a3a637af7f59
tunchunairarko <tunchunairarko@gmail.com>
2021-07-12 18:08:22 +0200

    jingle

--- src/containers/MediaContainer.js
+++ src/containers/MediaContainer.js
@@ -98,12 +98,13 @@ class MediaBridge extends Component {
     // with type=offer initially, it must do that during  the whole session
     this.pc = new RTCPeerConnection({iceServers: 
       [
-        { urls: 'stun:stun.l.google.com:19302' },
         {
           urls: ['turn:192.158.29.39:3478?transport=udp','turn:192.158.29.39:3478?transport=tcp'],
           credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
           username: '28224511:1379330808'
-        }
+        },
+        { urls: 'stun:stun.l.google.com:19302' }
+        
     ]});
     // when our browser gets a candidate, send it to the peer
     this.pc.onicecandidate = e => {
