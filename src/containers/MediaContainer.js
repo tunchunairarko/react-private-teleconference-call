d69f0920a86f6c801c8de965204db983dc7a5402
tunchunairarko <tunchunairarko@gmail.com>
2021-07-16 08:39:15 +0200

    Portia del Rossi

--- src/containers/MediaContainer.js
+++ src/containers/MediaContainer.js
@@ -180,12 +180,12 @@ class MediaBridge extends Component {
         {
           this.state.user==='host'? (<video className="remote-video" ref={(ref) => this.remoteVideo = ref} autoPlay></video>):
           (
-            // <div>
-            //   {/* <video style={{display:"none"}} className="remote-video" ref={(ref) => this.remoteVideo = ref} autoPlay></video> */}
-            //   <video className="remote-video" ref={(ref) => this.remoteVideo = ref} autoPlay></video>
-            //   <img className="remote-image" src="https://robotapi.isensetune.com/video_feed" />
-            // </div>
-            <video className="remote-video" ref={(ref) => this.remoteVideo = ref} autoPlay></video>
+            <div>
+              {/* <video style={{display:"none"}} className="remote-video" ref={(ref) => this.remoteVideo = ref} autoPlay></video> */}
+              <video className="remote-video" ref={(ref) => this.remoteVideo = ref} autoPlay></video>
+              <img className="remote-image" src="https://robotapi.isensetune.com/video_feed" />
+            </div>
+            // <video className="remote-video" ref={(ref) => this.remoteVideo = ref} autoPlay></video>
           )
         }
         
